b08cbe9f94489e17c78dd7403ac8b1ed3074dcf4
Gagan2601 <deepgagan9956@gmail.com>
2024-03-03 13:59:49 +0530

    Old React Project

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+    const host = "http://localhost:5000"
+    const notesInitial = []
+    const [notes, setNotes] = useState(notesInitial)
+
+    //Get all notes
+    const getNotes = async () => {
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": localStorage.getItem('token')
+            },
+        });
+        const json = await response.json()
+        setNotes(json)
+    }
+
+    //Add a note
+    const addNote = async (title, description, tag) => {
+
+        const response = await fetch(`${host}/api/notes/addnote`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": localStorage.getItem('token')
+            },
+            body: JSON.stringify({ title, description, tag }),
+        });
+        getNotes(response.json)
+    }
+    //Delete a note
+    const deleteNote = async (id) => {
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": localStorage.getItem('token')
+            }
+        });
+        getNotes(response.json)
+    }
+    //Edit a note
+    const editNote = async (id, title, description, tag) => {
+        const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": localStorage.getItem('token')
+            },
+            body: JSON.stringify({ title, description, tag }),
+        });
+        getNotes(response.json)
+    }
+    return (
+        <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState
